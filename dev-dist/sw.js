6a9de47cde625d3a2991702558c0b948ce3eec8d
huon.pham <huon.pham@avepoint.com>
2026-04-10 17:30:14 +0700

    fix(sw.js): update revision number for precached index.html

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.4j6i6gofi58"
+    "revision": "0.467r0persjg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {

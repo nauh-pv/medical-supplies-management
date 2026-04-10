3349d69df2f3e163eb4bc982735df56d6242af79
huon.pham <huon.pham@avepoint.com>
2026-04-10 10:04:41 +0700

    feat: update service worker revision and enhance Vercel configuration with manifest headers

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "apple-touch-icon.png", "masked-icon.svg"],
+      includeAssets: ["favicon.svg"],
       manifest: {
         name: "Medical Supplies Management",
         short_name: "MedSupply",

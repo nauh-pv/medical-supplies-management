65244749edbf1c22e98edf3db4fe8cb9d9aa0927
huon.pham <huon.pham@avepoint.com>
2026-04-12 17:47:28 +0700

    chore: update sw.js build artifact

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.9noqj8ha01"
+    "revision": "0.0e3d2vn0pjo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
